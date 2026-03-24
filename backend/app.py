9dac2f0e1499d68bfbd1e6b60fe151cd4331947a
Abu Hossain Mohammad Abed Hasan Tias <renken.tdkat@gmail.com>
2026-03-24 20:45:24 +0600

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -27,4 +27,7 @@ def index():
     return "Habit Tracker API is running"
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    # app.run(debug=True, port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
