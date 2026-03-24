bc4bad249a542c0dd51b4f7a6a0cf458aabb76cf
Abu Hossain Mohammad Abed Hasan Tias <renken.tdkat@gmail.com>
2026-03-24 21:42:05 +0600

    Change API base URL to production server

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://habit-tracker-pmhq.onrender.com/api',
+  // baseURL: 'http://localhost:5000/api',
 });
 
 api.interceptors.request.use((config) => {
